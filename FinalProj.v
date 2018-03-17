87fc59da192ac0999468532bf153a11dd5f87c9d
Sadi Muhammad Wali <sadi.wali@mail.utoronto.ca>
2018-03-17 13:20:31 -0400

    Modularized code. Improved tap detection, added dual hex score display up to 31, implemented pause and reset features.

--- FinalProj.v
+++ FinalProj.v
@@ -1,142 +1,322 @@
 
 
-module FinalProj(output [17:0] LEDR, input [17:0] SW, input CLOCK50, input [3:0] KEY);
+module FinalProj(output [17:0] LEDR, input [17:0] SW, input CLOCK50, input [3:0] KEY, output reg [7:0] HEX1, output reg [7:0] HEX0);
 	
-	single_lane sl(SW, CLOCK50, LEDR, KEY);
+	single_lane sl(SW, CLOCK50, KEY, LEDR,);
 
 
 endmodule
 
 // this is a single lane test
-
-
-module single_lane (
-		input [13:0] SW,
-		input clk,
-		output [9:0] LEDR,
-		input [3:0] KEY
-	);
+module single_lane (SW, clk, KEY, LEDR, HEXA, HEXB);
+	input [17:0] SW;
+	input clk;
+	input [3:0] KEY;
+	output [17:0] LEDR;
+	output [7:0] HEXA, HEXB;
 	
-	/********************************************************************************* variables */
-	// a lane represents a lane of notes
+	// game reset
+	wire reset = 0;
+	wire resetsw = 0;
+	assign resetsw = SW[1];
+
 	// mem is not changed
 	reg [99:0] lane_mem = 100'b0010100001000010000000000000000010000000010000000101000000000000000001001000001000000000000000000000;
-	// this changes as it is shifted right
+
+	// a lane represents a lane of notes, right shifted on every rate clock
 	reg [99:0] lane;
 
-	// program is running or paused
-	wire running = 1'b0;
+	// for storing the score (max is 31)
+	wire [4:0] score = 5'd0;
+	
+	// note movement clock
+	wire out_clk;
 
-	// for storing the score
-	reg [3:0] score = 4'd0;
-
-	/**************************************************************************** IO assignments */
 	// assign lane1 input to key 3
 	assign l1 = KEY[3];
-	// assign running state to switch 0, 1 for running, 0 for paused
-	assign running = SW[0];
+	
+	// program is running or paused
+	wire running = 0;
+	assign running = SW[0];  
+	
+	
 	// assign leds to display the last 10 bits of the lane only
 	assign LEDR [9:0] = lane[9:0];
 
 
-	// display the score
-	hex_display score_hex(score, HEX1);
-
-	// count to 50 million for 1Hz ( for moving notes along lane )
-	reg [25:0] rate_max = 26'b10111110101111000010000000;
-	reg [25:0] curr_count = 26'b0;
-	reg clk_out = 1'b0; // the 1hz signal
-	always @(posedge clk) begin
-		curr_count <= curr_count + 1;
-		if(curr_count == rate_max)
-			begin
-				curr_count <= 0;
-				clk_out <= !clk_out;
-			end	
+	// initialize the game
+	initial begin
+		// copy lane from memory to current game
+		lane = lane_mem;
 	end
 
-	// initialize the game
-	initial 
-		begin
-			// copy lane from memory to current game
+	
+	// on the 1 second clock, shift the register right ( move notes along lane )
+	always@(posedge out_clk, posedge resetsw) begin	
+		// if reset, reset
+		if (resetsw) begin
+			reset = 1;
 			lane = lane_mem;
 		end
-
-	// on key-press, check correctness if running, reset if paused
-	always@(posedge l1)
-		begin
-			if (running)
-				begin
-					if (lane[0] == 1'b1)
-						// hit key on time ( correct )
-						begin
-							// correct input, increment score by 1
-							score = score + 1;
-						end
-					else
-						// missed the key ( incorrect )
-						begin
-							// incorrect, subtract 2 from score, maintain positive
-							if (score == 4'd0 | score == 4'd1)
-								// cannot subtract 2, simply set to 0
-								score = 4'd0;
-							else
-								// can subtract 2, do it
-								score = score - 2;
-							
-						end
-				end
-			else	
-				// not running, so paused
-				begin
-					// reset the lane, forget the score
-					lane = lane_mem;
-					score = 4'b0;
-				end
-		end
-
-
-	// on the 1 second clock, shift the register right ( move notes along lane )
-	always@(posedge clk_out)
-		begin	
-			// if the game us running
-			if (running)
-				begin
-					// shift the bits
-					lane = 1 >> lane;
-				end
-			
-		end
+	
+		// if the game us running
+		if (running)
+			begin
+				// shift the bits
+				lane = 1 >> lane;
+			end
+	end
+	
+	
+	
+	// module declarations
+	note_rate_div nrd(clk, 25d'd10000000 , reset, out_clk); 
+	tap_detect lane1tap(l1, running, reset, score);
+	score_display(score, HEXA, HEXB);
+	
 	
 endmodule
 
-	
-module hex_display(IN, OUT);
-    input [3:0] IN;
-	 output reg [7:0] OUT;
-	 
-	 always @(*)
-	 begin
-		case(IN[3:0])
-			4'b0000: OUT = 7'b1000000;
-			4'b0001: OUT = 7'b1111001;
-			4'b0010: OUT = 7'b0100100;
-			4'b0011: OUT = 7'b0110000;
-			4'b0100: OUT = 7'b0011001;
-			4'b0101: OUT = 7'b0010010;
-			4'b0110: OUT = 7'b0000010;
-			4'b0111: OUT = 7'b1111000;
-			4'b1000: OUT = 7'b0000000;
-			4'b1001: OUT = 7'b0011000;
-			4'b1010: OUT = 7'b0001000;
-			4'b1011: OUT = 7'b0000011;
-			4'b1100: OUT = 7'b1000110;
-			4'b1101: OUT = 7'b0100001;
-			4'b1110: OUT = 7'b0000110;
-			4'b1111: OUT = 7'b0001110;
-			
-			default: OUT = 7'b0111111;
-		endcase
 
+module tap_detect(inp, running, reset, score);
+	input inp;  // switch input from user
+	input running;  // if game is running
+	input reset;  // reset switch resets score
+	output reg [4:0] score;  // score
+	
+	wire keyenable = 1;  // for disabling key on press down (prevent multi-presses)
+	// on key-press, check correctness if running, reset if paused
+	always@(posedge inp, negedge inp, posedge reset) begin
+		// enable the key on key up
+		if (!inp) begin
+			keyenable = 1;
+		end
+		
+		if (keyenable & running) begin
+			keyenable = 0; // disable key since key not released yet
+			if (lane[0] == 1'b1) begin
+				// correct input, increment score by 1
+				score = score + 1;
+			end	else begin
+				// incorrect, subtract 2 from score, maintain positive
+				if (score == 5'd0 | score == 5'd1)
+					// cannot subtract 2, simply set to 0
+					score = 5'd0;
+				else
+					// can subtract 2, do it
+					score = score - 2;
+			end			
+		end
+		
+		// reset the score
+		if (reset) begin
+			score = 5'd0;
+		end
 	end
+endmodule
+
+// module controls the speed of the notes
+module note_rate_div(clk, rate, reset, out_clk);
+	input clk;
+	input [25:0] rate;
+	input reset;
+	output reg out_clk;
+	
+	// count to given rate 
+	reg [25:0] curr_count = 26'b0;
+	out_clk = 0; // the signal
+	
+	always @(posedge clk) begin
+		if (reset) begin
+			// do nothing in reset mode
+			curr_count <= 0;
+			out_clk <= 0;
+		end else begin
+			curr_count <= curr_count + 1;
+			if (curr_count == rate) begin
+				curr_count <= 0;
+				// pulse out if not in reset mode
+				out_clk <= !out_clk;
+			end
+		end
+	end
+endmodule
+
+// module for displaying 5 bit score (max is 31)
+module score_display(score, HEXA, HEXB);
+	input [4:0] score;
+    output reg [7:0] HEXA, HEXB;
+     
+     always @(*)
+     begin
+        case(score)
+            5'd0:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b1000000;
+                end
+            5'd1:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b1111001;
+                end
+            6'd2:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b0100100;
+                end
+            5'd3:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b0110000;
+                end
+            5'd4:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b0011001;
+                end
+            5'd5:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b0010010;
+                end
+            5'd6:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b0000010;
+                end
+            5'd7:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b1111000;
+                end
+            5'd8:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b0000000;
+                end
+            5'd9:
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b0011000;
+                end
+            5'd10:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b1000000;
+                end
+            5'd11:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b1111001;
+                end
+            5'd12:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b0100100;
+                end
+            5'd13:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b0110000;
+                end
+            5'd14:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b0011001;
+                end
+            5'd15:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b0010010;
+                end
+            5'd16:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b0000010;
+                end
+            5'd17:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b1111000;
+                end
+            5'd18:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b0000000;
+                end
+            5'd19:
+                begin
+                    OUT1 = 7'b1111001;
+                    OUT2 = 7'b0011000;
+                end
+            5'd20:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b1000000;
+                end
+            5'd21:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b1111001;
+                end
+            5'd22:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b0100100;
+                end
+            5'd23:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b0110000;
+                end
+            5'd24:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b0011001;
+                end
+            5'd25:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b0010010;
+                end
+            5'd26:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b0000010;
+                end
+            5'd27:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b1111000;
+                end
+            5'd28:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b0000000;
+                end
+            5'd29:
+                begin
+                    OUT1 = 7'b0100100;
+                    OUT2 = 7'b0011000;
+                end
+            5'd30:
+                begin
+                    OUT1 = 7'b0110000;
+                    OUT2 = 7'b1000000;
+                end
+            5'd31:
+                begin
+                    OUT1 = 7'b0110000;
+                    OUT2 = 7'b0100100;
+                end
+           
+            default: // score is 0 by default
+                begin
+                    OUT1 = 7'b1000000;
+                    OUT2 = 7'b1000000;
+                end
+        endcase
+
+    end
 endmodule
