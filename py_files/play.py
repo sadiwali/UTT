df3dcfac67d0681dc931f943e7c18ed55cabbb52
Sadi Muhammad Wali <sadi.wali@mail.utoronto.ca>
2018-04-04 14:39:56 -0400

    fixed music playing script

--- py_files/play.py
+++ py_files/play.py
@@ -5,7 +5,27 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-playing = 0
+playing = 0 # is music playing?
+
+
+def kill_proc(pstring):
+    for line in os.popen('ps ax | grep ' + pstring + ' | grep -v grep'):
+        fields = line.split()
+        pid = fields[0]
+        os.kill(int(pid), signal.SIGKILL)
+
+def play():
+    global playing
+    playing = 1 
+    print('playing')
+    os.system('omxplayer -o local music.mp3 &')
+
+def stop():
+    global playing
+    playing = 0
+    print('stopping')
+    kill_proc('omxplayer.bin') 
+    kill_proc('omxplayer')
 
 
 
@@ -20,26 +40,4 @@ while True:
         if playing:
             stop()
     time.sleep(0.3)
-
-
-
-
-
-
-def kill_proc(pstring):
-    for line in os.popen('ps ax | grep ' + pstring + ' | grep -v grep'):
-        fields = line.split()
-        pid = fields[0]
-        os.kill(int(pid), signal.SIGKILL)
-
-def play():
-    print('playing')
-    playing = 1 
-    os.system('omxplayer -o local music.mp3 &')
-
-def stop():
-    print('stopping')
-    playing = 0
-    kill_proc('omxplayer.bin') 
-    kill_proc('omxplayer')
 
